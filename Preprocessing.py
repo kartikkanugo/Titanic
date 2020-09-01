c6125fb613fd5c63b9a07648595ba7a5ef7601be
kartikkanugo <karthik.kanugo@gmail.com>
2020-09-01 17:04:06 +0200

    Preprocessing

---
+++ Preprocessing.py
@@ -0,0 +1,29 @@
+# Import for data analysis
+
+import numpy as np
+import pandas as pd
+
+
+# Visualization import
+
+
+
+# Machinelearning import
+
+
+
+
+
+# Acquire Data
+
+
+
+# Analyze data
+
+
+
+#categorical features
+
+
+
+# Numerical features
