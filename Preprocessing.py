8d6d3799506224fe95f711bab219682b41c917dc
kartikkanugo <karthik.kanugo@gmail.com>
2020-09-02 14:14:54 +0200

    update

--- Preprocessing.py
+++ Preprocessing.py
@@ -5,25 +5,41 @@ import pandas as pd
 
 
 # Visualization import
-
+import matplotlib.pyplot as plt
 
 
 # Machinelearning import
-
+from sklearn.linear_model import LogisticRegression
 
 
 
 
 # Acquire Data
-
-
+train_df = pd.read_csv('./Data/train.csv')
+test_df = pd.read_csv('./Data/test.csv')
 
 # Analyze data
-
-
+# Below command helps to determine nulltype data
+#train_df.info()
 
 #categorical features
 
-
+data_distribution_all = train_df.describe(include = 'all')
+data_distribution_surv = train_df.describe(percentiles=[.61, .62,0.63,0.64]) 
+data_distribution_inc = train_df.describe(include=['O'])       
 
 # Numerical features
+
+
+
+
+
+
+# Mixed Features
+
+
+
+
+# Features with Errors
+
+
